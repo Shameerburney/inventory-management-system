e3b4aa03bd0bdfcd6998f38637444743bee414d1
Shameer Burney <shameerburney@example.com>
2025-11-28 22:01:00 +0500

    Use PORT environment variable

--- backend/main.py
+++ backend/main.py
@@ -348,4 +348,6 @@ def check_and_create_alert(db: Session, product: DBProduct):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
